f1a07eec9a9d66f3372b04f84fc7511f3d5dc484
ilovv <darkoilov28@yahoo.com>
2020-12-22 16:30:19 +0100

    Updates

---
+++ precache-manifest.a2be1b2fbfb4ea4519b8be1823bd481b.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c76455e86b742a70452dde7a5cbffab7",
+    "url": "/musicapp/index.html"
+  },
+  {
+    "revision": "3fc4ffadc83b71343d53",
+    "url": "/musicapp/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "1ddae3fc6fcf5f63f204",
+    "url": "/musicapp/static/css/main.f524a11e.chunk.css"
+  },
+  {
+    "revision": "3fc4ffadc83b71343d53",
+    "url": "/musicapp/static/js/2.99d61043.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/musicapp/static/js/2.99d61043.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ddae3fc6fcf5f63f204",
+    "url": "/musicapp/static/js/main.b65d3e66.chunk.js"
+  },
+  {
+    "revision": "80c5ff5466292b374baf",
+    "url": "/musicapp/static/js/runtime-main.202b1048.js"
+  },
+  {
+    "revision": "6ac2491a63e03d18ede817004b86dc15",
+    "url": "/musicapp/static/media/Girls-Listen-Music_0.6ac2491a.jpg"
+  },
+  {
+    "revision": "f7b0bcfae58e2e261ef63fa98654ac80",
+    "url": "/musicapp/static/media/among_stars.f7b0bcfa.jpg"
+  },
+  {
+    "revision": "56308c79037c8c6175f51683961fc7f9",
+    "url": "/musicapp/static/media/animals.56308c79.jpg"
+  },
+  {
+    "revision": "cbdfc7c3ecccb1358939181a70e5b2af",
+    "url": "/musicapp/static/media/band_of_gypsys.cbdfc7c3.jpg"
+  },
+  {
+    "revision": "01debc0702ea4ab2e66566b76ad6a680",
+    "url": "/musicapp/static/media/dark_side.01debc07.jpg"
+  },
+  {
+    "revision": "7a9616d4f5b0f13db9ab307a55726723",
+    "url": "/musicapp/static/media/electric-ladyland.7a9616d4.jpg"
+  },
+  {
+    "revision": "abda1b000f616eafdd94fde806234cc5",
+    "url": "/musicapp/static/media/erotica.abda1b00.jpg"
+  },
+  {
+    "revision": "c975bdc9e00221d7629c82bf76a0699d",
+    "url": "/musicapp/static/media/experienced.c975bdc9.jpg"
+  },
+  {
+    "revision": "8f1bac86841ba5640fa2589d21be6d68",
+    "url": "/musicapp/static/media/isle_of_wright.8f1bac86.jpg"
+  },
+  {
+    "revision": "2183c9290fad13def4bc95d85181847c",
+    "url": "/musicapp/static/media/jimi_hendrix.2183c929.jpg"
+  },
+  {
+    "revision": "00cb1649079c095ff473905f13a5d00f",
+    "url": "/musicapp/static/media/johnny_cash.00cb1649.jpg"
+  },
+  {
+    "revision": "8f08d776ab592c30e0fd61e9179140db",
+    "url": "/musicapp/static/media/like_a_virgin.8f08d776.jpg"
+  },
+  {
+    "revision": "3fc61baa2986dbbff145a7da793e6326",
+    "url": "/musicapp/static/media/madonna.3fc61baa.jpg"
+  },
+  {
+    "revision": "d45e9792a39a06b81afbb435913cc465",
+    "url": "/musicapp/static/media/man_comes_around.d45e9792.jpg"
+  },
+  {
+    "revision": "79d78f524e212bd34329f8f65a0a42d5",
+    "url": "/musicapp/static/media/no_grave.79d78f52.jpg"
+  },
+  {
+    "revision": "4dc3ec8cd55b9c5f879f4dcdf1defd7b",
+    "url": "/musicapp/static/media/pink_floyd.4dc3ec8c.jpg"
+  },
+  {
+    "revision": "c9d9333cdbfeff9713a691671a47b4f3",
+    "url": "/musicapp/static/media/porcupine_tree.c9d9333c.jpg"
+  },
+  {
+    "revision": "b7e1611ee6728a7029096bf05aa4029a",
+    "url": "/musicapp/static/media/ray_of_light.b7e1611e.jpg"
+  },
+  {
+    "revision": "846ce12ec0f25b7b03b4e5158337a658",
+    "url": "/musicapp/static/media/solitary_man.846ce12e.jpg"
+  },
+  {
+    "revision": "1df602755a8b863c5904d83d7e77839c",
+    "url": "/musicapp/static/media/the_wall.1df60275.jpg"
+  },
+  {
+    "revision": "182e471bd98a49e775b65258fdf57cc0",
+    "url": "/musicapp/static/media/true_blue.182e471b.jpg"
+  }
+]);
